50953002057bf0e2266763fb10c7e24f5854165c
Francesco Bellentani <Francesco.Bellentani@caditgroup.com>
2015-03-31 15:05:46 +0200

    Rimosse dipendenze inutili

--- esperimento01/esperimento01-web/src/main/java/com/cadit/formazione/web/rest/RestApi.java
+++ esperimento01/esperimento01-web/src/main/java/com/cadit/formazione/web/rest/RestApi.java
@@ -1,9 +1,9 @@
 package com.cadit.formazione.web.rest;
 
-import com.cadit.formazione.client.ServiceClient;
-import com.cadit.formazione.web.rest.render.Renderer;
+import com.cadit.formazione.api.ServiceApi;
 import com.cadit.formazione.api.data.DataObject;
 
+import javax.ejb.EJB;
 import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -15,13 +15,15 @@ import javax.ws.rs.QueryParam;
 @Path("/api")
 public class RestApi {
 
+    @EJB
+    ServiceApi _serviceApi;
+
     @GET
     @Path("service")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getDataObjects() throws NamingException {
-        ServiceClient client = new ServiceClient();
-        List<DataObject> dataObjects = client.getServiceApi().getDataObjects();
-        return Renderer.getJson(dataObjects);
+    public List<DataObject> getDataObjects() throws NamingException {
+        List<DataObject> dataObjects = _serviceApi.getDataObjects();
+        return dataObjects;
     }
     
     @GET
@@ -29,8 +31,7 @@ public class RestApi {
     public void addDataObject(
             @QueryParam("nome") String nome,
             @QueryParam("lingua") String lingua) throws NamingException {
-        ServiceClient client = new ServiceClient();
-        client.getServiceApi().addDataObject(nome, lingua);
+        _serviceApi.addDataObject(nome, lingua);
     }
     
 }
