2c1ebe1666c76326fa62ebd2b07fcac1dbedb978
Francesco Bellentani <Francesco.Bellentani@caditgroup.com>
2015-03-31 17:47:35 +0200

    Spostata lookup nel servizio rest

--- esperimento01/esperimento01-web/src/main/java/com/cadit/formazione/web/rest/RestApi.java
+++ esperimento01/esperimento01-web/src/main/java/com/cadit/formazione/web/rest/RestApi.java
@@ -3,20 +3,24 @@ package com.cadit.formazione.web.rest;
 import com.cadit.formazione.api.ServiceApi;
 import com.cadit.formazione.api.data.DataObject;
 
-import javax.ejb.EJB;
 import javax.naming.NamingException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import javax.naming.InitialContext;
 import javax.ws.rs.QueryParam;
 
 @Path("/api")
 public class RestApi {
 
-    @EJB
-    ServiceApi _serviceApi;
+    private final ServiceApi _serviceApi;
+    
+    public RestApi() throws NamingException {
+        InitialContext ic = new InitialContext();
+        _serviceApi = (ServiceApi) ic.lookup("java:global/esperimento01-ear/esperimento01-service-1.0-SNAPSHOT/EnterpriseService!com.cadit.formazione.api.ServiceApi");
+    }
 
     @GET
     @Path("service")
