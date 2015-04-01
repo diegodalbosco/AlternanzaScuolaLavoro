72aac372b6a51613283d90a18da64a3600cfdc5a
Eduard Andar <edd97ae@gmail.com>
2015-04-01 13:38:06 +0200

    Fix EduardApi (lookup)

--- esperimento01/esperimento01-web/src/main/java/com/cadit/formazione/web/rest/EduardRestApi.java
+++ esperimento01/esperimento01-web/src/main/java/com/cadit/formazione/web/rest/EduardRestApi.java
@@ -1,6 +1,6 @@
 package com.cadit.formazione.web.rest;
 
-import com.cadit.formazione.client.ServiceClient;
+import com.cadit.formazione.api.ServiceApi;
 import com.cadit.formazione.web.rest.render.Renderer;
 import com.cadit.formazione.api.data.DataObject;
 import com.cadit.formazione.api.eduard.Element;
@@ -11,6 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import javax.naming.InitialContext;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.QueryParam;
@@ -18,6 +19,13 @@ import javax.ws.rs.QueryParam;
 @Path("/Eduard")
 public class EduardRestApi {
 
+    private final ServiceApi _serviceApi;
+    
+    public EduardRestApi() throws NamingException {
+        InitialContext ic = new InitialContext();
+        _serviceApi = (ServiceApi) ic.lookup("java:global/esperimento01-ear/esperimento01-service-1.0-SNAPSHOT/EnterpriseService!com.cadit.formazione.api.ServiceApi");
+    }
+    
     @POST
     @Path("Element")
     public Element createElement( 
@@ -27,6 +35,7 @@ public class EduardRestApi {
                 )
             throws NamingException {
         //ServiceClient client = new ServiceClient();
+          
         Element element=new Element();
         element.setName(name);
         element.setSurname(surname);
@@ -41,8 +50,8 @@ public class EduardRestApi {
     @Path("service")
     @Produces(MediaType.APPLICATION_JSON)
     public String getDataObjects() throws NamingException {
-        ServiceClient client = new ServiceClient();
-        List<DataObject> dataObjects = client.getServiceApi().getDataObjects();
+        //ServiceClient client = new ServiceClient();
+        List<DataObject> dataObjects =  _serviceApi.getDataObjects();
         return Renderer.getJson(dataObjects);
     }
     
@@ -51,8 +60,8 @@ public class EduardRestApi {
     public void addDataObject(
             @QueryParam("nome") String nome,
             @QueryParam("lingua") String lingua) throws NamingException {
-        ServiceClient client = new ServiceClient();
-        client.getServiceApi().addDataObject(nome, lingua);
+        //ServiceClient client = new ServiceClient();
+       _serviceApi.addDataObject(nome, lingua);
     }
     
 }
