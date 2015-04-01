0645a1485159f03dfd29cca9b105f5895b25c29c
Diego Dal Bosco <diegodalbo97@gmail.com>
2015-04-01 11:56:15 +0200

    Aggiunto di nuovo package Api Dal Bosco

---
+++ esperimento01/esperimento01-api/src/main/java/com/cadit/formazione/api/dalbosco/DalBoscoApi.java
@@ -0,0 +1,10 @@
+package com.cadit.formazione.api.dalbosco;
+
+/**
+ *
+ * @author dalbosco
+ */
+public interface DalBoscoApi {
+    public String saluta(String saluto);
+    public String getRisposta(String domanda);
+}
